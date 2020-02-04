59df4f76846e0ad1f8e576646566129fffa87fdf
hector angulo <hectorangulo@macbook-pro.lan>
2020-02-04 11:45:02 -0800

    Created Collection Preview Component, shell for collection items.  Next lesson 71)

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-{
-  /* Functional component since don't need lifeccle methods  or store any state yet */
-}
 const HomePage = () => (
   <div className="homepage">
     <Directory />
