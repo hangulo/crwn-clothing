123790c8671cf645f13b8c3b1f2beff4ff2111ce
hector angulo <hectorangulo@macbook-pro.lan>
2020-02-05 12:51:29 -0800

    Added SignIn Form component

---
+++ src/pages/signin-page/signin-page.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/Sign-in.component";
+
+import "./signin-page.styles.scss";
+
+const SignInPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInPage;
