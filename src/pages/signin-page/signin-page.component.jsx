4783f51ee4f37789239045bd0857f97b0049697c
hector angulo <hectorangulo@macbook-pro.lan>
2020-02-05 21:50:41 -0800

    start of lesson 93-- although some CSS isues

--- src/pages/signin-page/signin-page.component.jsx
+++ src/pages/signin-page/signin-page.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/Sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import "./signin-page.styles.scss";
 
 const SignInPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
